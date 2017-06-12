e45e74148bb07aa25b040c29c62f77ed45dadac0
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-06-12 14:29:46 +0300

    Add functions to read processor VCC on AVR

--- src/VCC.h
+++ src/VCC.h
@@ -1,3 +1,11 @@
 #pragma once
 
 int readRawVccMilliVolts(uint8_t pin, float adcRefVoltage, long r1, long r2);
+
+
+#if defined(ARDUINO_ARCH_AVR)
+
+int readAtmegaInternalVcc();
+void readAtmegaInternalVccToStr(char* buf);
+
+#endif
